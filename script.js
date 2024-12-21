00a22033c2a481ed5a5274957b59513f7232e353
sarthakbahal <sarthakbahal.45@gmail.com>
2024-12-21 17:37:04 +0530

    Added script

---
+++ script.js
@@ -0,0 +1,25 @@
+
+async function getsongs() {
+    let x = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await x.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+             songs.push(element.href);   
+        }
+        
+    }
+    return songs;
+
+}
+
+async function main() {
+    let songs = await getsongs();
+    console.log(songs)
+}
+
+main();
