7fa9588421354449cc95617b2f1e72b8a5d79d21
sarthakbahal <sarthakbahal.45@gmail.com>
2024-12-26 21:44:54 +0530

    Updated Side bar

--- script.js
+++ script.js
@@ -117,7 +117,15 @@ async function main() {
         currsong.currentTime = seektime;
     });
 
+
+    document.querySelector(".hamburger").addEventListener("click", () =>{
+        document.querySelector(".left").style.left = "0";
+    })
     
+
+    document.querySelector(".close").addEventListener("click", () =>{
+        document.querySelector(".left").style.left = "-100%";
+    })
     
 }
 
